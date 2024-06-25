b1507ba6cfb1381d58397f8e20797bf78bb3f3b4
ckk914 <ckk9114@naver.com>
2024-06-25 11:31:50 +0900

    db 삭제 단일조회 전체 조회 테스트

--- src/main/java/com/spring/jpastudy/chap01/entity/Product.java
+++ src/main/java/com/spring/jpastudy/chap01/entity/Product.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
-@ToString
+@ToString(exclude = "nickName")   //⭐️중요~!
 @EqualsAndHashCode(of="id")  // 조금더 최적화에 유리 <<필드명 적기>>  예) (of="id","name")    id와 name이 같으면 같은객체이다. 같은 등등 설정 가능
 @NoArgsConstructor
 @AllArgsConstructor
@@ -32,7 +32,7 @@ public class Product {
     private Category category; // 상품 카테고리
 
     public enum Category{
-        FOOD, FASHION, ELECTROLIC,
+        FOOD, FASHION,  ELECTRONIC,
     }
     @CreationTimestamp  // insert 시 자동으로 서버시간 저장
     @Column(updatable = false)  //수정 불가
@@ -44,5 +44,17 @@ public class Product {
     //데이터베이스에는 저장안하고 클래스 내부에서만 사용할 필드
     @Transient
     private String nickName;
+
+    //컬럼 기본값 설정
+    @PrePersist
+    public void prePersist(){
+        if(this.price ==0){
+            this.price = 10000;
+        }
+        if(this.category == null){
+            this.category = Category.FOOD;
+        }
+    }
+
 
 }
--- src/test/java/com/spring/jpastudy/chap01/repository/ProductRepositoryTest.java
+++ src/test/java/com/spring/jpastudy/chap01/repository/ProductRepositoryTest.java
@@ -1,31 +1,66 @@
 package com.spring.jpastudy.chap01.repository;
 
 import com.spring.jpastudy.chap01.entity.Product;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+import static com.spring.jpastudy.chap01.entity.Product.Category.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
-@Rollback               //실제 db에 넣지 않기 위함
+//@Transactional
+//@Rollback               //실제 db에 넣지 않기 위함
 class ProductRepositoryTest {
 
     @Autowired
     ProductRepository productRepository;
+    @BeforeEach
+    void insertBeforeTest(){
+        Product p1 = Product.builder()
+                .name("아이폰")
+                .category(ELECTRONIC)
+                .price(2000000)
+                .build();
+        Product p2 = Product.builder()
+                .name("탕수육")
+                .category(FOOD)
+                .price(20000)
+                .build();
+        Product p3 = Product.builder()
+                .name("구두")
+                .category(FASHION)
+                .price(300000)
+                .build();
+        Product p4 = Product.builder()
+                .name("주먹밥")
+                .category(FOOD)
+                .price(1500)
+                .build();
+
+        productRepository.save(p1);
+        productRepository.save(p2);
+        productRepository.save(p3);
+        productRepository.save(p4);
+    }
 
     @Test
     @DisplayName("상품을 db에 저장한다")
     void saveTest() {
         //given
         Product product = Product.builder()
-                .name("신발")
-                .price(140000)
-                .category(Product.Category.FASHION)
+                .name("순대")
+//                .price(140000)
+//                .category(Product.Category.FASHION)
                 .build();
         //when
         //insert 후 저장된 데이터의 객체를 반환
@@ -34,5 +69,50 @@ class ProductRepositoryTest {
 
         //then
         assertNotNull(saved);
+    }
+    //
+
+    @Test
+    @DisplayName("1번 상품을 삭제")
+    void deleteTest() {
+        //given
+        Long id = 1L;
+        //when
+        productRepository.deleteById(id);
+        //then
+      Product foundProduct = productRepository.findById(id)
+                .orElse(null);
+        assertNull(foundProduct);
+    }
+    //
+    @Test
+    @DisplayName("3번 상품을 단일 조회하면 그 상품명이 구두이다.")
+    void findOneTest() {
+        
+        //given
+        Long id = 3L;
+        //when
+        Product foundproduct = productRepository.findById(id).orElse(null);
+        
+        assertEquals("구두",foundproduct.getName());
+        System.out.println("foundproduct = " + foundproduct);
+
+        //then
+    }
+
+    @Test
+    @DisplayName("조회된 데이터는 4이다.")
+    void findAllTest() {
+        //given
+
+
+        //when
+        List<Product> productList = productRepository.findAll();
+        //then
+        System.out.println("\n\n\n");
+        productList.forEach(System.out::println);
+        System.out.println("\n\n\n");
+
+        assertEquals(4,productList.size());
     }
 }
