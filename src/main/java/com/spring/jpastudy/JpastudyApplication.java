08268125cd564507d71551ebbe9bc2aac64ee9d5
ckk914 <ckk9114@naver.com>
2024-06-25 09:38:30 +0900

    init commit

---
+++ src/main/java/com/spring/jpastudy/JpastudyApplication.java
@@ -0,0 +1,13 @@
+package com.spring.jpastudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpastudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpastudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/jpastudy/JpastudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jpastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpastudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
